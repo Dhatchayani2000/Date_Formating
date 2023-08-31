8810db7a53b4aad09db4b2cb75ad69dab55cb074
Dhatchayani2000 <141829867+Dhatchayani2000@users.noreply.github.com>
2023-08-31 11:57:46 +0530

    Delete Date Formating.py

--- Date Formating.py
+++
@@ -1,8 +0,0 @@
-import datetime
-def givendate(DateFormat):
-   date=datetime.datetime.strptime(DateFormat, "%Y/%m/%d")
-   resultdate=date.strftime("%A,%Y %b %d")
-   return resultdate
-
-DateFormat=input("Enter The Date :")
-print(givendate(DateFormat))
